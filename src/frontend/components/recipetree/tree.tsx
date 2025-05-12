35ca3f92874e5adea448b38ea683f1d72788a5b6
susTuna <hidasetsuna@gmail.com>
2025-05-12 18:31:38 +0700

    chore: resizing

--- src/frontend/components/recipetree/tree.tsx
+++ src/frontend/components/recipetree/tree.tsx
@@ -2,17 +2,17 @@
 'use client';
 import ReactFlow from 'reactflow';
 import 'reactflow/dist/style.css';
-import { parseRecipeJson, RecipeJson } from '@/util/parser/parser';
+import { parseRecipeJson, RecipeJson, edgeTypes } from '@/util/parser/parser';
 
 interface RecipeTreeProps {
   recipeJson: RecipeJson;
 }
 
 const RecipeTree: React.FC<RecipeTreeProps> = ({ recipeJson }) => {
-  const { flowNodes, flowEdges, edgeTypes } = parseRecipeJson(recipeJson);
+  const { flowNodes, flowEdges } = parseRecipeJson(recipeJson);
 
   return (
-    <div style={{ width: '90%', height: '80vh' }}>
+    <div style={{ width: '90%', height: '62.5vh' }}>
       <ReactFlow nodes={flowNodes} edges={flowEdges} edgeTypes={edgeTypes} fitView>
       </ReactFlow>
     </div>
