d56de495047c1dce79818c7b705853754630f9b8
susTuna <hidasetsuna@gmail.com>
2025-05-12 18:33:16 +0700

    feat: search enabled

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,20 +1,66 @@
 "use client"
 
+import { useState } from "react"
 import SearchBar from "@/components/searchbar/searchbar"
 import RecipeTree from "@/components/recipetree/tree"
+import { fetchElementInfo } from "@/util/parser/parser"
 
-const recipeData = {
-  nodes: ["Earth", "Air", "Dust", "Fire", "Gunpowder", "Fire", "Explosion", "Fire", "Fire", "Energy", "Atomic Bomb"],
-  dependencies: [
-    {node: 10, dependency1: 6, dependency2: 9},
-    {node: 2, dependency1: 0, dependency2: 1},
-    {node: 4, dependency1: 2, dependency2: 7},
-    {node: 9, dependency1: 3, dependency2: 8},
-    {node: 6, dependency1: 5, dependency2: 4}
-  ]
-};
+interface ElementInfo {
+  id: number;
+  name: string;
+  tier: number;
+  image_url: string;
+}
 
 export default function Page() {
+  const [recipeData, setRecipeData] = useState(null)
+  const [treeKey, setTreeKey] = useState(0)
+
+  const handleSearch = async (requestBody: any) => {
+    try {
+      const response = await fetch("http://localhost:5761/fullrecipe/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+      })
+  
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
+  
+      const data = await response.json()
+      const searchId = data.search_id
+  
+      pollRecipeData(searchId)
+    } catch (error) {
+      console.error("Fetch error:", error)
+    }
+  }
+  
+  const pollRecipeData = async (searchId: number) => {
+    const interval = setInterval(async () => {
+      try {
+        const response = await fetch(`http://localhost:5761/fullrecipe/${searchId}`)
+        if (!response.ok) throw new Error("Failed to fetch recipe data")
+  
+        const data = await response.json()
+        
+        const uniqueIds = Array.from(new Set<number>(data.nodes));
+        const nameMap = await fetchElementInfo(uniqueIds);
+
+        setRecipeData({ ...data, nameMap });
+  
+        if (data.finsihed) {
+          clearInterval(interval)
+        }
+      } catch (error) {
+        console.error("Polling error:", error)
+        clearInterval(interval)
+      }
+    }, 250) // poll every 250ms
+  }
+  
+
   return (
     <div className="bg-hero-pattern bg-center bg-black bg-blend-darken bg-opacity-70 flex min-h-screen flex-col items-center justify-start pt-10 gap-8">
       <div className="flex flex-col items-center h-1/4">
@@ -22,8 +68,8 @@ export default function Page() {
           Little Alchemy 2 Recipe Finder
         </h1>
       </div>
-      <SearchBar />
-      <RecipeTree recipeJson={recipeData} />
+      <SearchBar onSearch={handleSearch} />
+      {recipeData && <RecipeTree recipeJson={recipeData} />}
     </div>
   )
 }
