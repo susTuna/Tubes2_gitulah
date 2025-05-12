bab4f933519944fa5e95267eeca283e5f202dec7
susTuna <hidasetsuna@gmail.com>
2025-05-12 18:28:58 +0700

    feat: CORS middleware

--- src/backend/service/middleware/corsmiddleware.go
+++ src/backend/service/middleware/corsmiddleware.go
@@ -8,7 +8,7 @@ import (
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Allow any origin
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
