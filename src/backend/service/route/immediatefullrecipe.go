a1dd0116f2fd60d30cc5403558df85c2ae89b677
Arlow5761 <arlow5761@gmail.com>
2025-05-13 08:04:45 +0700

    refactor: simplify immediate full recipe logic

--- src/backend/service/route/immediatefullrecipe.go
+++ src/backend/service/route/immediatefullrecipe.go
@@ -5,57 +5,30 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/filbertengyo/Tubes2_gitulah/database"
 	"github.com/filbertengyo/Tubes2_gitulah/schema"
 	"github.com/filbertengyo/Tubes2_gitulah/service/findfullrecipe"
 )
 
 func ImmediateFullRecipe(w http.ResponseWriter, r *http.Request) {
-	var request schema.SearchRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadGateway)
-		return
-	}
-
-	if !request.Valid() {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	element, err := database.FindElementById(request.Element)
+	resp, err := http.Post("localhost:5761/fullrecipe", "application/json", r.Body)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	var response schema.SearchResponse
+	var searchResponse schema.SearchResponse
+	err = json.NewDecoder(resp.Body).Decode(&searchResponse)
+	resp.Body.Close()
 
-	if request.Method == "dfs" && request.Threading == "single" {
-		response.SearchID = findfullrecipe.WithSinglethreadedDFS(element, request.Count, request.Delay)
-	} else if request.Method == "dfs" && request.Threading == "multi" {
-		response.SearchID = findfullrecipe.WithMultithreadedDFS(element, request.Count, request.Delay)
-	} else if request.Method == "bfs" && request.Threading == "single" {
-		response.SearchID = findfullrecipe.WithSinglethreadedBFS(element, request.Count, request.Delay)
-	} else if request.Method == "bfs" && request.Threading == "multi" {
-		response.SearchID = findfullrecipe.WithMultithreadedBFS(element, request.Count, request.Delay)
-	} else {
-		w.WriteHeader(http.StatusNotImplemented)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	searchResult := findfullrecipe.FindSearch(int(response.SearchID))
-
-	if searchResult == nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
+	for !findfullrecipe.FindSearch(searchResponse.SearchID).Finished {
+		time.Sleep(1 * time.Millisecond)
 	}
 
-	for !searchResult.Finished {
-		time.Sleep(time.Millisecond * 1)
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Add("Content-Type", "application/json")
-	w.Write([]byte(searchResult.Serialize()))
+	GetFullRecipe(w, r)
 }
