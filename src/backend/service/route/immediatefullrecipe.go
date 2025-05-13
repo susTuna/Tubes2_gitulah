8f5e4c21d5055c270bd57d74b6352e198890a191
Arlow5761 <arlow5761@gmail.com>
2025-05-13 10:40:40 +0700

    feat: add multi threaded dfs

--- src/backend/service/route/immediatefullrecipe.go
+++ src/backend/service/route/immediatefullrecipe.go
@@ -2,18 +2,21 @@ package route
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"time"
+
+	"fmt"
 
 	"github.com/filbertengyo/Tubes2_gitulah/schema"
 	"github.com/filbertengyo/Tubes2_gitulah/service/findfullrecipe"
 )
 
 func ImmediateFullRecipe(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Post("localhost:5761/fullrecipe", "application/json", r.Body)
+	resp, err := http.Post("http://localhost:5761/fullrecipe/", "application/json", r.Body)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(resp.StatusCode)
 		return
 	}
 
@@ -30,5 +33,21 @@ func ImmediateFullRecipe(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Millisecond)
 	}
 
-	GetFullRecipe(w, r)
+	resp, err = http.Get("http://localhost:5761/fullrecipe/" + fmt.Sprint(searchResponse.SearchID))
+
+	if err != nil {
+		w.WriteHeader(resp.StatusCode)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(body)
 }
