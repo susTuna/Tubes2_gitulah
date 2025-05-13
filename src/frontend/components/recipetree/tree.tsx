51c32c5657c6c7681c4ac4c65dc7cbe6d73dc744
susTuna <hidasetsuna@gmail.com>
2025-05-13 18:36:22 +0700

    feat: multi yey

--- src/frontend/components/recipetree/tree.tsx
+++ src/frontend/components/recipetree/tree.tsx
@@ -1,6 +1,7 @@
 // components/RecipeTree.tsx
 'use client';
 import ReactFlow from 'reactflow';
+import { Controls } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { parseRecipeJson, RecipeJson, edgeTypes, nodeTypes } from '@/util/parser/parser';
 
@@ -9,7 +10,19 @@ export default function RecipeTree({ recipeJson } : { recipeJson : RecipeJson}) 
 
   return (
     <div style={{ width: '90%', height: '62.5vh' }}>
-      <ReactFlow nodes={flowNodes} edges={flowEdges} edgeTypes={edgeTypes} nodeTypes={nodeTypes} fitView>
+      <ReactFlow nodes={flowNodes} edges={flowEdges} edgeTypes={edgeTypes} nodeTypes={nodeTypes} fitView
+        nodesDraggable={false}
+        nodesConnectable={false}
+        elementsSelectable={false}
+        nodesFocusable={false}
+        panOnScroll={true}
+        zoomOnScroll={true}
+        zoomOnPinch={true}
+        panOnDrag={true}
+        selectionOnDrag={false}
+        preventScrolling={false}
+      >
+        <Controls />
       </ReactFlow>
     </div>
   );
